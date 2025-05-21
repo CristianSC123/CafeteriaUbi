c4fe0467d4d4ef67c8a2e9d632d14576df14476d
Cristian <cristiaansantacruz@gmail.com>
2025-05-21 11:24:10 -0400

    Corrigiendo los models de productos

--- apps/Productos/admin.py
+++ apps/Productos/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-admin.site.register()
-admin.site.register(Categoria)
+from .models import Categoria, Producto
+
 # Register your models here.
+admin.site.register(Categoria)
+admin.site.register(Producto)
+
--- apps/Productos/models.py
+++ apps/Productos/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 import uuid
+
+class Categoria(models.Model):
+    id_categoria = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    nombre = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f"{self.nombre}"
+
+    class Meta:
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
+
 class Producto(models.Model):
     id_producto = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     nombre = models.CharField(max_length=100)
@@ -7,24 +19,15 @@ class Producto(models.Model):
     url_imagen = models.URLField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    disponible = models.BooleanField(default=True)
         
     def __str__(self):
-        return {self.nombre + self.precio}
+        return f"{self.nombre} {self.precio}"
 
     class Meta:
         verbose_name = "Producto"
         verbose_name_plural = "Productos"
         
-class Categoria(models.Model):
-    id_categoria = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    nombre = models.CharField(max_length=100)
-    
-    def __str__(self):
-        return {self.nombre}
-
-    class Meta:
-        verbose_name = "Categoria"
-        verbose_name_plural = "Categorias"
 
 
 # Create your models here.
