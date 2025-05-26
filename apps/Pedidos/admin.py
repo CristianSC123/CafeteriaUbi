ef4de73dd50db4a8e5aa732942e2c8a28c414372
Juan <juan@gmail.com>
2025-05-25 20:18:21 -0400

    Pedidos

--- apps/Pedidos/admin.py
+++ apps/Pedidos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Pedido
 # Register your models here.
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ('id_pedido', 'id_cliente', 'id_producto', 'cantidad', 'fecha_pedido', 'fecha_entrega', 'estado')
+    
