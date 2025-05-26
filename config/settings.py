1f7fe2a36523d02cd90cf4c724670fa8917b6f25
Cristian <cristiaansantacruz@gmail.com>
2025-05-26 11:00:10 -0400

    agregando las credenciales de la bd en railway

--- config/settings.py
+++ config/settings.py
@@ -79,11 +79,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'dLqRZxpMCDNKXdjEHHKGHuAdMclyaGqE',
+        'HOST': 'tramway.proxy.rlwy.net',  
+        'PORT': '43728'    
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
