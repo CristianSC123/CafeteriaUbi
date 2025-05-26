571745c9cd31c9e0cb7061a1cd258af54d352d5d
Cristian <cristiaansantacruz@gmail.com>
2025-05-26 10:29:43 -0400

    Uniendo todas las ramas

--- apps/Productos/models.py
+++ apps/Productos/models.py
@@ -11,11 +11,12 @@ class Categoria(models.Model):
     class Meta:
         verbose_name = "Categoria"
         verbose_name_plural = "Categorias"
+        db_table = "categorias"
 
 class Producto(models.Model):
     id_producto = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     nombre = models.CharField(max_length=100)
-    descripcion = models.TextField(max_length=200)
+    descripcion = models.TextField(max_length=200, blank=True, null=True)
     url_imagen = models.URLField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
@@ -27,8 +28,7 @@ class Producto(models.Model):
     class Meta:
         verbose_name = "Producto"
         verbose_name_plural = "Productos"
+        db_table = "productos"
         
-
-
 # Create your models here.
 
